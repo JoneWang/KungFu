de2e7a9f4fd457ae4cc8f25e5a9f9cc66db2a2a0
JoneWang <tuya@foxmail.com>
2012-05-12 12:58:31 +0800

    modify manager.py

--- code/KungFu/manager.py
+++ code/KungFu/manager.py
@@ -1,5 +1,9 @@
-import sys
-from kungfu.server.manager import execute 
+#!/usr/bin/python
+# coding=utf-8
 
-if __mian__=='__main__':
-    sys.argv
+import sys
+import os
+from kungfu.server.manager import execute
+
+if __mian__ == '__main__':
+    execute(sys.argv, os.path.abspath(os.path.dirname(__file__)))
